20374c25b0fcd6ce05ff2681b1cbe52d1bebdda5
John <jmseymour111@gmail.com>
2017-07-07 10:33:33 -0700

    Knex setup, ready to integrate PostgreSQL database

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
-var index = require('./routes/index');
+// Route Requires
+const index = require('./routes/index');
 
-var app = express();
+// Express App
+const app = express();
 
-// view engine setup
+// View Engine Setup (Handlebars)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(allowCrossDomain)
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// App Routes
 app.use('/', index);
+
+// CORS Cross Domain
+function allowCrossDomain (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+  if ('OPTIONS' == req.method) {
+    res.send(200)
+  } else {
+    next()
+  }
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
---
+++ knexfile.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/internet_of_stings',
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  }
+}
