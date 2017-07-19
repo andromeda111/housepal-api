0ca2b0f0cad261003bca46d8e865039046485fe1
John <jmseymour111@gmail.com>
2017-07-18 21:40:35 -0700

    Message board will clear posts beyond number 50

--- routes/messageboard.js
+++ routes/messageboard.js
@@ -30,15 +30,29 @@ router.post('/', passport.authenticate('jwt', { session: false}), function(req, 
 
     const newMsg = {posterId: decoded.id, posterName: decoded.name, content: req.body.content, postTime: {postTime: req.body.postTime}, house_id: decoded.house_id}
 
-    console.log(newMsg);
 
-    db('message_board').insert(newMsg).returning('*').then(result => {
-      console.log(result);
-      res.json(result)
+    db('message_board').where({house_id: decoded.house_id}).then(allMsgs => {
+      let houseMsgs = allMsgs
+      if (houseMsgs.length > 50) {
+        houseMsgs.sort((a, b) => {
+          return a.id - b.id;
+        })
+        db('message_board').where({id: houseMsgs[0].id}).del().returning('*').then(deleted => {
+        }).catch(err => {
+          console.log(err);
+        })
+      }
+      db('message_board').insert(newMsg).returning('*').then(result => {
+        res.json(result)
+      }).catch(err => {
+        console.log(err);
+        res.status(400).send({success: false, msg: 'Error. Try again.', err: err});
+      })
     }).catch(err => {
       console.log(err);
       res.status(400).send({success: false, msg: 'Error. Try again.', err: err});
     })
+
   } else {
     return res.status(403).send({success: false, msg: 'No token provided.'});
   }
