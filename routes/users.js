26c1759fea6c14ceb40fd9a93af53431315feca7
John <jmseymour111@gmail.com>
2017-07-20 11:58:09 -0700

    removing passport middleware on device token route for testing

--- routes/users.js
+++ routes/users.js
@@ -64,7 +64,7 @@ router.put('/leave/:id', passport.authenticate('jwt', { session: false}), functi
   }
 });
 
-router.put('/updateDeviceToken', passport.authenticate('jwt', { session: false}), function(req, res, next) {
+router.put('/updateDeviceToken', function(req, res, next) {
   let token = getToken(req.headers);
   if (token) {
     let decoded = jwt.decode(token, process.env.JWT_SECRET);
